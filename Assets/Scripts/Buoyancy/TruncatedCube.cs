1705611d44ca4ce0e6c104b7b9747e45b12a1ddf
Juan Manuel Guerrero Valadez <0211637@up.edu.mx>
2023-03-15 22:05:36 -0600

    Volume calculation

--- Assets/Scripts/Buoyancy/TruncatedCube.cs
+++ Assets/Scripts/Buoyancy/TruncatedCube.cs
@@ -10,16 +10,18 @@ public class TruncatedCube {
 
     private readonly float xzPlane;
     private readonly List<Vector3> cubeVertices;
+    private readonly Vector3 center;
     private readonly List<List<Vector3>> faces;
     private readonly HashSet<Vector3> newFace;
     private readonly Vector3[] normals;
 
-    public TruncatedCube(float xzPlane, Vector3[] vertices) {
+    public TruncatedCube(float xzPlane, Vector3[] vertices, Vector3 center) {
         this.cubeVertices = new(vertices);
         this.normals = new Vector3[6];
         this.faces = new();
         this.newFace = new();
         this.xzPlane = xzPlane;
+        this.center = center;
     }
 
     public List<List<Vector3>> Faces {
@@ -83,7 +85,6 @@ public class TruncatedCube {
     private void AddNeighborIntesection(Vector3 vertex, Vector3 neighbor, List<Vector3> updatedVertices)
     {
         if (vertex.y == neighbor.y) {
-            Debug.LogWarning("Prevented a division by zero");
             return;
         }
         if (neighbor.y <= this.xzPlane){
@@ -115,17 +116,80 @@ public class TruncatedCube {
         PolygonToTriangles(vertices, outlist);
     }
 
+    private bool GetNewFaceTriangles(List<Vector3[]> outList){
+        if (this.newFace.Count > 0) {
+            FaceToTriangles(this.newFace.ToList(), outList, false);
+            return true;
+        }
+        return false;
+    }
+
     public List<Vector3[]> GetTriangles(){
         List<Vector3[]> triangles = new();
         foreach (List<Vector3> face in faces) {
             FaceToTriangles(face, triangles, true);
         }
-        if(this.newFace.Count > 0) {
-            FaceToTriangles(this.newFace.ToList(), triangles, false);
-        }
+        GetNewFaceTriangles(triangles);
         return triangles;
     }
 
+    private Vector3 GetIntersectionPoint(){
+        return new Vector3(this.center.x, this.xzPlane, this.center.z);
+    }
+
+    public float GetVolume()
+    {
+        List<Vector3[]> triangles = GetTriangles();
+        Vector3 intersectionPoint = GetIntersectionPoint();
+        float totalVolume = 0f;
 
 
+        foreach (Vector3[] triangle in triangles) {
+            Vector3 normal = Vector3.Cross(triangle[1] - triangle[0], triangle[2] - triangle[0]);
+
+            Vector3 toKnownPoint = intersectionPoint - triangle[0];
+            if (Vector3.Dot(normal, toKnownPoint) < 0) {
+                Vector3 temp = triangle[1];
+                triangle[1] = triangle[2];
+                triangle[2] = temp;
+            }
+            totalVolume += GetTetrahedronVolume(intersectionPoint, triangle, normal);
+        }
+
+        return totalVolume;
+    }
+
+    public float GetPrismVolume(float top) {
+        List<Vector3[]> triangles = new();
+        if (!GetNewFaceTriangles(triangles)){
+            return 0f;
+        }
+        float height = top - this.xzPlane;
+        float totalVolume = 0f;
+        foreach (Vector3[] triangle in triangles) {
+            totalVolume += GetTriangleArea(triangle);
+        }
+        return totalVolume * height;
+    }
+
+    private float GetTetrahedronVolume(Vector3 intersectionPoint, Vector3[] triangle, Vector3 normal)
+    {
+        Vector3 base1 = triangle[1] - triangle[0];
+        Vector3 base2 = triangle[2] - triangle[0];
+        Vector3 heightVec = intersectionPoint - triangle[0];
+        float baseArea = 0.5f * Vector3.Cross(base1, base2).magnitude;
+        float height = Mathf.Abs(Vector3.Dot(heightVec, normal.normalized));
+        float volume = (1f / 3f) * baseArea * height;
+        if (volume < 0f) {
+            Debug.LogWarning("Got a negative volume: " + volume);
+        }
+        return volume;
+    }
+
+    private float GetTriangleArea(Vector3[] triangle){
+        float b = Vector3.Distance(triangle[0], triangle[1]);
+        Vector3 half = (triangle[0] + triangle[1]) * 0.5f;
+        float h = Vector3.Distance(half, triangle[2]);
+        return b * h / 2;
+    }
 }
